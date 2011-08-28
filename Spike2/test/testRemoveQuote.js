5f4e6747f8752505eea57495b7616079d9654ce9
Dave <david.bethke@gmail.com>
2011-08-28 15:34:48 -0500

    more tests, more misunderstanding on getKeyTasks

--- Spike2/test/testRemoveQuote.js
+++ Spike2/test/testRemoveQuote.js
@@ -2,12 +2,25 @@ TestRemoveQuote = TestCase('testRemoveQuote');
 
 TestRemoveQuote.prototype.setUp= function(){
 	// create an array of task objects
+	var propNames={0:'project',1:'story',2:'coder',3:'task',4:'iteration',5:'complexity'};
 	taskList=new Array();
 	for(var i=0; i<10;i++){
 		var task= new Object();
 		task={project:'project0',story:'story'+i,coder:'coder'+i,task:'task'+i,iteration:'0',complexity:'2'};
 		taskList[i]=task;
-	};
+	}
+	// build a tasklist w/ split string
+	taskList2=new Array();
+	for(var i=0; i < 10; i++){
+		var task2=new Object();
+		var propVals='project0'+','+'story'+i+','+'coder'+i+','+'task'+i+','+'0'+'2';
+		var splitResults= propVals.split(',');
+		for(var k=0; k<splitResults.length;k++){
+			task2[propNames[k]]=splitResults[k];
+		}
+		taskList2[i]=task2;
+		
+	}
 	
 };
 TestRemoveQuote.prototype.tearDown=function(){
@@ -53,6 +66,23 @@ TestRemoveQuote.prototype.testGetKeyTasksVal=function(){
 	var subTaskList= new Array();
 	//subTaskList=taskList;
 	subTaskList=getKeyTasks(taskList,'project','project0');
-	results=getKeyTasks(subTaskList,'coder','coder8')
+	results=getKeyTasks(subTaskList,'coder','coder8');
+	assertEquals('coder9',results[0]['coder']);
+};
+TestRemoveQuote.prototype.testTaskList2Length=function(){
+	assertEquals(10,taskList2.length);
+};
+TestRemoveQuote.prototype.testTaskList2Val=function(){
+	var val=['task8'];
+	assertEquals(val[0],taskList2[8]['task']);
+};
+TestRemoveQuote.prototype.testGetKeyTasksLength2=function(){
+	var val=['task8'];
+	var results=getKeyTasks(taskList2,'project','project0');
+	assertEquals(11,results.length);
+};
+TestRemoveQuote.prototype.testGetKeyTasksVal2=function(){
+	var val='coder8';
+	results=getKeyTasks(taskList2,'coder',val);
 	assertEquals('coder9',results[0]['coder']);
 };
