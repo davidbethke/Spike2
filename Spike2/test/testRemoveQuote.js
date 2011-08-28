0c9ea8cc655bef95424c11d907c9bb72431b952b
Dave <david.bethke@gmail.com>
2011-08-28 13:59:18 -0500

    getting something to work

--- Spike2/test/testRemoveQuote.js
+++ Spike2/test/testRemoveQuote.js
@@ -1,11 +1,27 @@
 TestRemoveQuote = TestCase('testRemoveQuote');
 
-TestRemoveQuote.prototype.setUp(){
-	var name= 'hello';
-}
-TestRemoveQuote.prototype.tearDown(){
+TestRemoveQuote.prototype.setUp= function(){
+	// create an array of task objects
+	taskList=new Array();
+	for(var i=0; i<10;i++){
+		var task= new Object();
+		task={project:'project0',story:'story'+i,coder:'coder'+i,task:'task'+i,iteration:'0',complexity:'2'};
+		taskList[i]=task;
+	};
 	
-}
-TestRemoveQuote.prototype.testRemoveQuote(){
+};
+TestRemoveQuote.prototype.tearDown=function(){
+	
+};
+TestRemoveQuote.prototype.testRemoveQuote=function(){
+	var name= '"hello"';
 	assertEquals('hello',removeQuote(name));
-}
+};
+TestRemoveQuote.prototype.testGetTotal=function(){
+	var val=[0];
+	assertEquals('20',getTotal(taskList,'iteration',val));
+};
+TestRemoveQuote.prototype.testGetCount=function(){
+	var val=[0];
+	assertEquals(10,getCount(taskList,'iteration',val));
+};
